955fce88f67f90eeedb7230101af6b43c9b16c58
Emircan Küçükmotor <emircan.kucukmotor92@gmail.com>
2019-05-24 18:16:25 +0300

    Added pagination to home/explore view funcs.

--- app/routes.py
+++ app/routes.py
@@ -18,8 +18,19 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POST_PER_PAGE'], False)
     return render_template('index.html', title='Home Page', form=form, posts=posts)
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    return render_template('index.html', title='Explore', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -127,9 +138,4 @@ def unfollow(username):
     return redirect(url_for('user', username=username))
 
 
-@app.route('/explore')
-@login_required
-def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
 

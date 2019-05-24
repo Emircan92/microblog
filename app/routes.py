952edd1db654b8c11c21b17e270c546d8c9c9033
Emircan Küçükmotor <emircan.kucukmotor92@gmail.com>
2019-05-24 20:27:24 +0300

    First steps of trying to implement PasswordREsetRequestEmailService.

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,9 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm
 from app.models import User, Post
+from app.email import send_password_reset_email
 from datetime import datetime
 
 
@@ -12,13 +13,12 @@ from datetime import datetime
 @login_required
 def index():
     form = PostForm()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            post = Post(body=form.post.data, author=current_user)
-            db.session.add(post)
-            db.session.commit()
-            flash('Your post is now live!')
-            return redirect(url_for('index'))
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
@@ -153,4 +153,16 @@ def unfollow(username):
     return redirect(url_for('user', username=username))
 
 
-
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password.')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['emircan.kucukmotor@gmail.com']
-    POSTS_PER_PAGE = 3
+    ADMINS = ['emircantester@gmail.com']
+    POSTS_PER_PAGE = 10
